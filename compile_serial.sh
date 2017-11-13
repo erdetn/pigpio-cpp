d5c1033da36253e53760f1e217b2bc71cb1b23fc
Erdet N <2962934+erdetn@users.noreply.github.com>
2017-11-13 23:56:00 +0100

    Update compile_serial.sh

--- compile_serial.sh
+++ compile_serial.sh
@@ -1,1 +1,1 @@
-g++ -std=c++03 -Wall -I / -pthread -o ex_serial.build ex_serial.cpp csession.cpp cserial.cpp -lpigpiod_if2 -lrt
+g++ -std=c++03 -Wall -I / -pthread -o ex_serial.build ex_serial.cpp cclientsession.cpp cserial.cpp -lpigpiod_if2 -lrt
